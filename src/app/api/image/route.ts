082e52c0e925f05a75b8130a19edcc6727cb1c34
RobertoHortaFilho <robertohortafilho@gmail.com>
2024-10-25 12:21:49 -0300

    fix: use nextRequest

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const data = await req.json()
   console.log(data)
   return NextResponse.json({message: 'ok'})  
